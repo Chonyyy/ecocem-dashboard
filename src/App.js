984a3ad1f9058ac58b81187459333dd17d851252
Chonyyy <mlchony7@gmail.com>
2024-06-18 21:46:52 -0600

    mantenimiento templating

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ import NewUser from "./pages/NewUser";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import NewProduct from "./pages/NewProduct";
+
 import AccionMantenimiento from './pages/mantenimiento/AccionMantenimiento';
+import Equipo from './pages/mantenimiento/Equipo';
+import Herramientas from './pages/mantenimiento/Herramientas';
+import OrdenTrabajo from './pages/mantenimiento/OrdenTrabajo';
+import Reporte from './pages/mantenimiento/Reporte';
+import RoturaEquipo from './pages/mantenimiento/RoturaEquipo';
+import TipoRotura from './pages/mantenimiento/TipoRotura';
 
 function App() {
   return (
@@ -19,6 +26,12 @@ function App() {
         <Sidebar />
         <Routes>
           <Route path='/accion-mantenimiento' element={<AccionMantenimiento />} />
+          <Route path='/equipo' element={<Equipo />} />
+          <Route path='/herramientas' element={<Herramientas />} />
+          <Route path='/orden-trabajo' element={<OrdenTrabajo />} />
+          <Route path='/reporte' element={<Reporte />} />
+          <Route path='/rotura-equipo' element={<RoturaEquipo />} />
+          <Route path='/tipo-rotura' element={<TipoRotura />} />
 
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
