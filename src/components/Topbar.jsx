e5556d0a655bf5cdd295fa2a8668b6b723815cac
Chonyyy <mlchony7@gmail.com>
2024-07-19 20:00:01 -0600

    "Added react-toastify dependency and implemented toast notifications in App.js and Topbar.jsx components"

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,21 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import '../css/component/topbar.css'
+import React, { useState, useEffect, useRef } from 'react';
+import '../css/component/topbar.css';
 import { checkAdmin } from '../scripts/auth';
 import { NotificationsNone, Language, Settings } from "@mui/icons-material";
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
 
 function Topbar() {
+  const [administrator, setAdministrator] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+  const hasShownNotifications = useRef(false);
 
-  const [administrator, setAdministrator] = useState(false);//TODO: Aplicar cambios en el sitio dependiendo de si se es adminsitrador o no 
+  // Notify function that displays all notifications
+  const notify = () => {
+    if (!hasShownNotifications.current) {
+      notifications.forEach(notification => {
+        toast(notification.message); // Assuming each notification has a `message` field
+      });
+      hasShownNotifications.current = true;
+    }
+  };
 
   useEffect(() => {
     const checkAuthenticated = async () => {
       if (await checkAdmin()) {
         setAdministrator(true);
       }
-    }
-
-    checkAuthenticated()
+    };
+    fetchNotification();
+    checkAuthenticated();
   }, []);
+
+  const fetchNotification = async () => {
+    try {
+      const response = await axios.get('http://localhost:5103/api/Notification');
+      const data = response.data;
+      if (Array.isArray(data)) {
+        setNotifications(data); // Store notifications in state
+      } else {
+        setNotifications([]); // Ensure it's an array
+      }
+      hasShownNotifications.current = false; // Reset shown notifications status after fetching new data
+    } catch (error) {
+      toast.error("Error de red");
+      setNotifications([]); // In case of error, ensure it's still an array
+    }
+  };
 
   return (
     <div className="topbar">
@@ -24,9 +53,10 @@ function Topbar() {
           <span className="logo">ECOCEM</span>
         </div>
         <div className="topRight">
-          <div className="topbarIconContainer">
+          <div className="topbarIconContainer" onClick={notify}>
             <NotificationsNone />
-            <span className="topIconBadge">2</span>
+            <ToastContainer />
+            <span className="topIconBadge">{notifications.length}</span>
           </div>
           <div className="topbarIconContainer">
             <Language />
